0a97ec38b830635156d81f015c872aa2b18f065e
Aline Ramalho <aline.ramalho@ibm.com>
2022-08-19 17:51:24 -0300

    alteracoes 64%

--- aula-projeto/src/app/app.module.ts
+++ aula-projeto/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Novatransferencia } from './nova-transferencia/nova-transferencia.component';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { from } from 'rxjs';
 
@@ -7,7 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -21,7 +24,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
     FormsModule,
 
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue:'BRL',
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
